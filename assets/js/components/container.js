1d05bcec10b0a8feda7a79b7c0dbb3ae32ffbb2e
Juan Torres <junete190@gmail.com>
2024-07-25 08:06:15 -0500

    Avances versión desktop

--- assets/js/components/container.js
+++ assets/js/components/container.js
@@ -1,8 +1,11 @@
 function container () {
   setTimeout(function() {
     var content = document.querySelector('.container');
+    var body = document.body
+
     content.classList.remove("hidden");
     content.classList.add("visible")
+    body.classList.remove('no-scroll')
   }, 3000)
 }
 
